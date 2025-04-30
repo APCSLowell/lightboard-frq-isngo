81c9b1cca5e659d4eecf7cab9e37e6083bfcf00b
isngo <isngo@s.sfusd.edu>
2025-04-30 10:00:35 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -11,6 +11,16 @@ public class LightBoard
   public LightBoard(int numRows, int numCols)
   {
     /* to be implemented in part (a) */
+    lights = new boolean[numRows][numCols];
+    for(int r = 0; r<numRows; r++){
+      for(int c = 0; c<numCols; c++){
+        if(Math.random()<0.4){
+          lights[r][c] = true;
+        } else {
+          lights[r][c] = false;
+        }
+      }
+    }
 
   }
 
@@ -21,6 +31,19 @@ public class LightBoard
   public boolean evaluateLight(int row, int col)
   {
     /* to be implemented in part (b) */
+    int count = 0;
+    for(int r = 0; r<lights.length; r++){
+      if(lights[r][col]){
+        count++;
+      }
+    }
+    if(lights[row][col]==true && count%2==0){
+      return false;
+    } else if (lights[row][col]==false && count%3==0){
+      return true;
+    } else {
+      return lights[row][col];
+    }
    
  
   }
